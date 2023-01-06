bccdedc4e0f49bfcffcb8df9cee8e4ac8fdaf10c
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-01-06 11:00:32 -0300

    Funções de calculo prontas

---
+++ calcule.js
@@ -0,0 +1,14 @@
+let cal = {
+    soma: ((x, y) => {
+        return x + y
+    }),
+    dim:((x, y) => {
+        return x + y
+    }),
+    mult:((x, y) => {
+        return x * y
+    }),
+    div:((x, y) => {
+        return x / y
+    }),
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+let argum = process.argv.slice(2)
+
+
+let a  = argum[1];
+let b = argum[2];
+let c = ''
+
+
+
+
+if(argum[0] === 's'){
+    c = cal.soma(a,b)
+}else if(argum[0] === '-'){
+    c = cal.dim(a,b)
+}else if(argum[0] === 'm'){
+    c = cal.mult(a,b)
+}else if(argum[0] === 'd'){
+    c = cal.div(a,b)
+}
